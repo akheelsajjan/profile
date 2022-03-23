b3d4f0e3f08a0df67241734e95095ab8fc3e21fa
Akheel Sajjan <akheel.sajjan@microfocus.com>
2022-03-23 10:57:51 +0530

    Micro focus expreince added

--- js/main.js
+++ js/main.js
@@ -128,6 +128,23 @@ function initMap() {
     }
   }
 
+  function myFunctionMF() {
+    var dots = document.getElementById("dots4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+  
+    if (dots.style.display === "none") {
+
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
   function myFunctionMPS(){
     var dots = document.getElementById("dots2");
     var moreText = document.getElementById("more2");
